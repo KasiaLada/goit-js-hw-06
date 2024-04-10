dd21d5d4a50bf639bbde937a8603356b852516a8
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-10 18:59:58 +0200

    zrobione zadanie 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,27 @@
-const numbersOfList = document.querySelectorAll(".item")
-let count = numbersOfList.length;
+const numbersOfCategory = document.querySelectorAll('.item');
+let count = numbersOfCategory.length;
 console.log(`Number of categories: ${count}`);
+// ---------------------------
+
+const categoryItems = document.querySelectorAll('#categories .item');
+
+for (const item of categoryItems) {
+	const header = item.querySelector('h2').textContent;
+	const numberOfListItems = item.querySelectorAll('ul li').length;
+	console.log(`Category: ${header}`);
+	console.log(`Elements: ${numberOfListItems}`);
+}
+// Drugi sposób, nie wiem, który lepszy?
+
+// const categoryItems = document.querySelectorAll('#categories .item');
+
+// console.log(`Number of categories: ${categoryItems.length}`);
+
+
+// categoryItems.forEach(item => {
+//   const header = item.querySelector('h2').textContent;  
+//   const numberOfListItems = item.querySelectorAll('ul li').length; 
+
+//   console.log(`Category: ${header}`);
+//   console.log(`Elements: ${numberOfListItems}`);
+// });
