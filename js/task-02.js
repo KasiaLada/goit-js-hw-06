9984230c43a0f04d1d322db157f5b0bbb6608668
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-11 12:48:14 +0200

    zrobione zadanie 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const listFragment = document.createElement('ul');
+
+ingredients.forEach((ingredient) => {
+	const listItem = document.createElement('li');
+	listItem.textContent = ingredient;
+	listItem.classList.add('item');
+	listFragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(listFragment);
