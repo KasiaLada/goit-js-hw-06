7f54437c1b96862faf1f66fe57e1e782914fcf1a
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-15 11:41:32 +0200

    Zrobione zadanie 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value; 
+    const password = event.currentTarget.elements.password.value; 
+
+   
+    if (email === '' || password === '') {
+        alert("Please fill in all fields."); 
+        return;
+    }
+
+ 
+    const formData = {
+        email,
+        password
+    };
+
+    console.log(formData); 
+
+    loginForm.reset(); 
+});
