7da2a1072783635baea9c73bf25dedeaffd74e9e
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-04-15 12:24:58 +0200

    Zrobione zadanie 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const button = document.querySelector('button');
+const span = document.querySelector('span');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const colorChanger = () => {
+	const newColor = getRandomHexColor();
+	document.body.style.backgroundColor = newColor;
+	span.textContent = newColor;
+};
+
+button.addEventListener('click', colorChanger);
